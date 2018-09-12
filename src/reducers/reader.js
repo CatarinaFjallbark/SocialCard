e6a1c82c0a9fd6d0bc5910c4acc4febbd8b36b53
Catarina Sörensen <catarinajayway@Catarinas-MacBook-Pro.local>
2018-09-12 10:33:35 +0200

    new reducer reader

---
+++ src/reducers/reader.js
@@ -0,0 +1,68 @@
+import cwitter from "../resourses/cwitter.json"
+
+export const REMOVE_REQUESTED = 'REMOVE_REQUESTED'
+export const REMOVE = 'REMOVE'
+export const ADD_REQUESTED = 'ADD_REQUESTED'
+export const ADD = 'ADD'
+
+
+function creatInitialState() {
+    let list_of_cweets_hard = [];
+    list_of_cweets_hard = cwitter.list_of_sc.slice(0);;
+    return { list_of_cweets_hard };
+}
+
+const initialState = creatInitialState();
+
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case REMOVE_REQUESTED:
+            return {
+                list_of_cweets: state.list_of_cweets.map(cweet => cweet.id === action.id ? { ...cweet, isIncrementing: true } : cweet)
+            }
+
+        case REMOVE:
+            return {
+                list_of_cweets: state.list_of_cweets.map(cweet => cweet.id === action.id ? { ...cweet, isIncrementing: false, thumb: cweet.thumb + 1 } : cweet)
+            }
+        case ADD_REQUESTED:
+            return {
+                list_of_cweets: state.list_of_cweets.map(cweet => cweet.id === action.id ? { ...cweet, isIncrementing: true } : cweet)
+            }
+
+        case ADD:
+            return {
+                list_of_cweets: state.list_of_cweets.map(cweet => cweet.id === action.id ? { ...cweet, isIncrementing: false, thumb: cweet.thumb + 1 } : cweet)
+            }
+        default:
+            return state
+    }
+}
+
+export const addAC = (id) => {
+    return dispatch => {
+        dispatch({
+            type: ADD_REQUESTED,
+            id: id
+        })
+
+        dispatch({
+            type: ADD,
+            id: id
+        })
+    }
+}
+export const removeAC = (id) => {
+    return dispatch => {
+        dispatch({
+            type: REMOVE_REQUESTED,
+            id: id
+        })
+
+        dispatch({
+            type: REMOVE,
+            id: id
+        })
+    }
+}  
